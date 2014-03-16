e684ddc921af3aa7498380dace0b2465bd2a4c7a
Giacomo Macrì <gamacri2000@gmail.com>
2014-03-16 14:32:22 +0100

    most fine options

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,9 +5,12 @@
 nodes_count = Settings.database.nodes.count
 RiakRailsAuthentication::Application.config.session_store :"ripple_session_store", { 
   nodes:  Settings.database.nodes.map(&:to_hash), 
-  n_val: nodes_count -1 , 
+  n_val: nodes_count, 
   r: nodes_count - 1, 
-  w:  nodes_count ,
+  w:  nodes_count - 1,
+  rw: nodes_count - 1,
+  dw: nodes_count - 1,
+  pw: nodes_count - 1,
   # This last two options are set according to riak doc, where this combination make sense in case of session usage 
   # http://docs.basho.com/riak/latest/theory/concepts/Vector-Clocks/
   last_write_wins: true,
