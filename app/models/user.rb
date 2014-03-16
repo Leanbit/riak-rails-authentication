9d57b37194332c48982a6954abd395973c02ef1e
Giacomo Macrì <gamacri2000@gmail.com>
2014-03-16 18:59:46 +0100

    add secondary level index to list all users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,14 @@ class User
 
   validates :email, presence: true
 
+  #TODO all this method can/must be moved into BasicModel
   def store
     client = riak_client
     self.id ||= give_uuid
     user = client[self.class.name.downcase.pluralize].get_or_new(self.id)
     user.data = self.attributes
+    user.indexes['email_bin'] << self.email
+    user.indexes['joined_int'] << Time.now.utc.to_i
     user.store
   end
 
@@ -22,8 +25,17 @@ class User
     valid? ? store : false
   end
 
+  def self.all
+    client = riak_client
+    bucket = client[self.name.downcase.pluralize]
+    users = bucket.get_index('joined_int', 0..Time.now.utc.to_i)
+    users.map { |u| parse_from_riak(bucket[u]) }
+  end
 
-  def parse_from_riak
+
+  def self.parse_from_riak(rkobject)
+    data = rkobject.data
+    new(data)
   end
 
 end
